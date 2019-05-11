2985a53e63a69014b7bafe8e0fed18485f5860d3
Konstantin Vitoshnev <kvmultiship@yandex-team.ru>
2019-05-11 20:10:30 +0300

    init

---
+++ test/src/test_print_ip.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE print_ip_test
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(print_ip_test_suite)
+
+    BOOST_AUTO_TEST_CASE(init_test) {
+        BOOST_CHECK(true == true);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
